8be86c35a24baf9ba7bc537635b0ef882ad66eb5
linab309 <lin_309@163.com>
2020-05-12 11:31:52 +0800

    test 提交

---
+++ v1000_mass/Projects/Mass_Storage/MDK-ARM/STM32L152-EVAL/bmp280.d
@@ -0,0 +1,5 @@
+.\stm32l152-eval\bmp280.o: ..\src\HARDWARE\BMP\bmp280.c
+.\stm32l152-eval\bmp280.o: ..\src\HARDWARE\BMP\bmp280.h
+.\stm32l152-eval\bmp280.o: ..\src\HARDWARE\BMP\bmp280_defs.h
+.\stm32l152-eval\bmp280.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\stm32l152-eval\bmp280.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stddef.h
